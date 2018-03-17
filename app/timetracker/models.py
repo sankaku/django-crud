4ef5b5541ef6255f45dad0e807546a2063b7ff02
sankaku <sankaku.git@gmail.com>
2018-03-17 15:30:02 +0900

    add models(Project, Task)

--- app/timetracker/models.py
+++ app/timetracker/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    """
+    Types of projects.
+
+    name: the name of the project
+    color: color code of the project. This is for displaying projects in view page.
+    """
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=6)
+
+
+class Task(models.Model):
+    """
+    A single record of doing project.
+
+    start_time: start time
+    end_time: end time
+    project: the project that the task belongs to
+    content: specific content
+    """
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    project = models.ForeignKey(Project, on_delete=models.PROTECT)
+    content = models.CharField(max_length=200)
