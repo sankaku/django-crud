ac2d57214c237e50899f6b3788adba69792e4c1d
sankaku <sankaku.git@gmail.com>
2018-03-18 22:46:01 +0900

    impl `add_task`(except for validation)

--- app/timetracker/urls.py
+++ app/timetracker/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'timetracker'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('task/new/', views.add_task, name='add_task'),
+    path('task/create/', views.create_task, name='create_task'),
 ]
--- app/timetracker/views.py
+++ app/timetracker/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Project, Task
+from django.urls import reverse
+from datetime import datetime
 
 
 def index(request):
@@ -10,3 +12,35 @@ def index(request):
     }
 
     return render(request, 'timetracker/index.html', context)
+
+
+def add_task(request):
+    project_list = Project.objects.order_by('id')
+    now = datetime.now().strftime("%Y-%m-%dT%H:%M")
+    context = {
+        'project_list': project_list,
+        'now': now,
+    }
+
+    return render(request, 'timetracker/add_task.html', context)
+
+
+def create_task(request):
+    try:
+        project_id = request.POST['project']
+        project = get_object_or_404(Project, pk=project_id)
+        start_time = request.POST['start_time']
+        end_time = request.POST['end_time']
+        content = request.POST['content']
+        task = Task(project=project, start_time=start_time,
+                    end_time=end_time, content=content)
+        task.save()
+    except (IndexError) as e:
+        project_list = Project.objects.order_by('id')
+        context = {
+            'error_message': 'Incorrect input!\n{0}\n{1}'.format(e.args),
+            'project_list': project_list,
+        }
+        return render(request, 'timetracker/add_task.html', context)
+    else:
+        return HttpResponseRedirect(reverse('timetracker:index'))
